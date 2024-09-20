f29d65acf20669ac4869b2f7063294a6a92f769b
Engezerstorung <154867622+Engezerstorung@users.noreply.github.com>
2024-09-20 04:25:13 +0200

    Implement a system to get color-corrected color palettes at assembly time.
    NOT AN INGAME OPTION.
    In includes.asm turn "DEF LCD_COLORS EQU 0" to 1 to enable the functionality and play with the options.
    All options can be turned OFF independently.
    Color matrix : allow to choose 1 of 3 color matrixes to correct the colors
    Gamma : apply a gamma value of your choice (the value must be in the 1.0 format, dont omit the decimal even for integers)
    White Point : define the max white value, give a hue to the image, can be used to mimic a screen tone, come ;with 3 predefined profiles, more can be added from this file.
    For the tinkerers, the rest is in macros\gfx.asm.

--- includes.asm
+++ includes.asm
@@ -1,6 +1,52 @@
 DEF GEN_2_GRAPHICS EQU 1
 DEF ALT_PARTY_MENU_COLOR EQU 1
 
+DEF LCD_COLORS EQU 0
+	;; Color Options
+	; 0 to turn OFF all color modification
+
+DEF COLOR_MATRIX = 1
+	;; Color correction
+	; 0 to turn OFF, 1 to 2 for 3 options (values above 3 will default to 1)
+	; 1 : Personal one based of hunterk with values rounded up to single digits
+	; 2 : hunterk Color Mangler matrix from their gbc shader
+	; 3 : Jojobear13 matrix from their "Built-in Color Correction for GBC games" tutorial
+
+
+DEF GAMMA = 1.3
+	; 0 or 1.0 to turn OFF
+	; Don't omit the .0 for integer numbers
+
+DEF WHITE_POINT = 0
+	; 0 to turn OFF, value above defined profiles will default to 0
+	; 1 : Personal lightish white point
+	; 2 : Lighter profile proposed in BGB to emulate the GBC screen reflector hue
+	; 3 : Darker profile proposed in BGB to emulate the GBC screen reflector hue
+
+
+;; White point profiles
+; Values  0-31 or $00-$1F
+IF WHITE_POINT == 1 ; Personal lightish white point
+	DEF wRed = 30
+	DEF wGreen = 31
+	DEF wBlue = 28
+
+ELIF WHITE_POINT == 2 ; Lighter BGB white point
+	DEF wRed = 26
+	DEF wGreen = 27
+	DEF wBlue = 24
+
+ELIF WHITE_POINT == 3 ; Lighter BGB white point
+	DEF wRed = 16
+	DEF wGreen = 17
+	DEF wBlue = 15
+
+; You can add more white point profiles following those exemples.
+ELSE
+	REDEF WHITE_POINT = 0
+ENDC
+
+
 INCLUDE "macros/asserts.asm"
 INCLUDE "macros/const.asm"
 INCLUDE "macros/predef.asm"
