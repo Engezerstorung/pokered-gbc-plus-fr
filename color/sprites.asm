db42a53dc6fc03fc2c883abb09ab3f74a78b40d0
Engezerstorung <154867622+Engezerstorung@users.noreply.github.com>
2025-07-15 13:24:56 +0200

    Fixes + Color Plus Update

--- color/sprites.asm
+++ color/sprites.asm
@@ -213,7 +213,7 @@ GetPartySpritePalette:
 	ld [wPokedexNum], a ; Store a in wram to be used in the function
 	farcall IndexToPokedex
 	ld a, [wPokedexNum] ; Get the result of the function
-	cp 152 ; check for and ID higher than Mew's
+	cp NUM_POKEMON + 1 ; check for an ID higher than Mew's
 	jr c, .notAboveMew ; Jump if not higher than Mew's
 	xor a ; if higher than Mew's then give ID 0 so that purple palette is assigned
 .notAboveMew
