d7377730a8098b56170aeac8a88f2d63f6e726a9
dannye <33dannye@gmail.com>
2025-07-19 14:53:53 -0500

    Use more updated hardware constants

--- color/oak_intro.asm
+++ color/oak_intro.asm
@@ -32,10 +32,10 @@ GotPalID:
 	ld d, a
 
 	ld a, 2
-	ldh [rSVBK], a
+	ldh [rWBK], a
 	CALL_INDIRECT LoadSGBPalette
 	xor a
-	ldh [rSVBK], a
+	ldh [rWBK], a
 	ret
 
 ; Load red sprite palette at the end of Oack Speech, before the player map sprite appear
