pret/pokered

Use `dname` macro to automatically pad names with `"@"` characters (#525)

data/events/trades.asm
@@ -1,7 +1,12 @@
+MACRO npctrade
+; give mon, get mon, dialog id, nickname
+	db \1, \2, \3
+	dname \4, NAME_LENGTH
+ENDM
+
 TradeMons:
 ; entries correspond to TRADE_FOR_* constants
 	table_width 3 + NAME_LENGTH
-	; give mon, get mon, dialog id, nickname
 	; The two instances of TRADE_DIALOGSET_EVOLUTION are a leftover
 	; from the Japanese Blue trades, which used species that evolve.
 	; TRADE_DIALOGSET_EVOLUTION did not refer to evolution in Japanese
@@ -9,14 +14,14 @@ TradeMons:
 	; "went and evolved" and also changed the trades to match. English
 	; Red/Blue uses the original JP Red/Green trades but with the JP Blue
 	; post-trade text.
-	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "TERRY@@@@@@"
-	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
-	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
-	db PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "SAILOR@@@@@"
-	db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "DUX@@@@@@@@"
-	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC@@@@@@@"
-	db POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "LOLA@@@@@@@"
-	db RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "DORIS@@@@@@"
-	db VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "CRINKLES@@@"
-	db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "SPOT@@@@@@@"
+	npctrade NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "TERRY"
+	npctrade ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL"
+	npctrade BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU" ; unused
+	npctrade PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "SAILOR"
+	npctrade SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "DUX"
+	npctrade SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "MARC"
+	npctrade POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "LOLA"
+	npctrade RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "DORIS"
+	npctrade VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "CRINKLES"
+	npctrade NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "SPOT"
 	assert_table_length NUM_NPC_TRADES

data/pokemon/names.asm
@@ -1,193 +1,193 @@
 MonsterNames::
 	table_width NAME_LENGTH - 1
-	db "RHYDON@@@@"
-	db "KANGASKHAN"
-	db "NIDORAN♂@@"
-	db "CLEFAIRY@@"
-	db "SPEAROW@@@"
-	db "VOLTORB@@@"
-	db "NIDOKING@@"
-	db "SLOWBRO@@@"
-	db "IVYSAUR@@@"
-	db "EXEGGUTOR@"
-	db "LICKITUNG@"
-	db "EXEGGCUTE@"
-	db "GRIMER@@@@"
-	db "GENGAR@@@@"
-	db "NIDORAN♀@@"
-	db "NIDOQUEEN@"
-	db "CUBONE@@@@"
-	db "RHYHORN@@@"
-	db "LAPRAS@@@@"
-	db "ARCANINE@@"
-	db "MEW@@@@@@@"
-	db "GYARADOS@@"
-	db "SHELLDER@@"
-	db "TENTACOOL@"
-	db "GASTLY@@@@"
-	db "SCYTHER@@@"
-	db "STARYU@@@@"
-	db "BLASTOISE@"
-	db "PINSIR@@@@"
-	db "TANGELA@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "GROWLITHE@"
-	db "ONIX@@@@@@"
-	db "FEAROW@@@@"
-	db "PIDGEY@@@@"
-	db "SLOWPOKE@@"
-	db "KADABRA@@@"
-	db "GRAVELER@@"
-	db "CHANSEY@@@"
-	db "MACHOKE@@@"
-	db "MR.MIME@@@"
-	db "HITMONLEE@"
-	db "HITMONCHAN"
-	db "ARBOK@@@@@"
-	db "PARASECT@@"
-	db "PSYDUCK@@@"
-	db "DROWZEE@@@"
-	db "GOLEM@@@@@"
-	db "MISSINGNO."
-	db "MAGMAR@@@@"
-	db "MISSINGNO."
-	db "ELECTABUZZ"
-	db "MAGNETON@@"
-	db "KOFFING@@@"
-	db "MISSINGNO."
-	db "MANKEY@@@@"
-	db "SEEL@@@@@@"
-	db "DIGLETT@@@"
-	db "TAUROS@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "FARFETCH'D"
-	db "VENONAT@@@"
-	db "DRAGONITE@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "DODUO@@@@@"
-	db "POLIWAG@@@"
-	db "JYNX@@@@@@"
-	db "MOLTRES@@@"
-	db "ARTICUNO@@"
-	db "ZAPDOS@@@@"
-	db "DITTO@@@@@"
-	db "MEOWTH@@@@"
-	db "KRABBY@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "VULPIX@@@@"
-	db "NINETALES@"
-	db "PIKACHU@@@"
-	db "RAICHU@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "DRATINI@@@"
-	db "DRAGONAIR@"
-	db "KABUTO@@@@"
-	db "KABUTOPS@@"
-	db "HORSEA@@@@"
-	db "SEADRA@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "SANDSHREW@"
-	db "SANDSLASH@"
-	db "OMANYTE@@@"
-	db "OMASTAR@@@"
-	db "JIGGLYPUFF"
-	db "WIGGLYTUFF"
-	db "EEVEE@@@@@"
-	db "FLAREON@@@"
-	db "JOLTEON@@@"
-	db "VAPOREON@@"
-	db "MACHOP@@@@"
-	db "ZUBAT@@@@@"
-	db "EKANS@@@@@"
-	db "PARAS@@@@@"
-	db "POLIWHIRL@"
-	db "POLIWRATH@"
-	db "WEEDLE@@@@"
-	db "KAKUNA@@@@"
-	db "BEEDRILL@@"
-	db "MISSINGNO."
-	db "DODRIO@@@@"
-	db "PRIMEAPE@@"
-	db "DUGTRIO@@@"
-	db "VENOMOTH@@"
-	db "DEWGONG@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "CATERPIE@@"
-	db "METAPOD@@@"
-	db "BUTTERFREE"
-	db "MACHAMP@@@"
-	db "MISSINGNO."
-	db "GOLDUCK@@@"
-	db "HYPNO@@@@@"
-	db "GOLBAT@@@@"
-	db "MEWTWO@@@@"
-	db "SNORLAX@@@"
-	db "MAGIKARP@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MUK@@@@@@@"
-	db "MISSINGNO."
-	db "KINGLER@@@"
-	db "CLOYSTER@@"
-	db "MISSINGNO."
-	db "ELECTRODE@"
-	db "CLEFABLE@@"
-	db "WEEZING@@@"
-	db "PERSIAN@@@"
-	db "MAROWAK@@@"
-	db "MISSINGNO."
-	db "HAUNTER@@@"
-	db "ABRA@@@@@@"
-	db "ALAKAZAM@@"
-	db "PIDGEOTTO@"
-	db "PIDGEOT@@@"
-	db "STARMIE@@@"
-	db "BULBASAUR@"
-	db "VENUSAUR@@"
-	db "TENTACRUEL"
-	db "MISSINGNO."
-	db "GOLDEEN@@@"
-	db "SEAKING@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "PONYTA@@@@"
-	db "RAPIDASH@@"
-	db "RATTATA@@@"
-	db "RATICATE@@"
-	db "NIDORINO@@"
-	db "NIDORINA@@"
-	db "GEODUDE@@@"
-	db "PORYGON@@@"
-	db "AERODACTYL"
-	db "MISSINGNO."
-	db "MAGNEMITE@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "CHARMANDER"
-	db "SQUIRTLE@@"
-	db "CHARMELEON"
-	db "WARTORTLE@"
-	db "CHARIZARD@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "ODDISH@@@@"
-	db "GLOOM@@@@@"
-	db "VILEPLUME@"
-	db "BELLSPROUT"
-	db "WEEPINBELL"
-	db "VICTREEBEL"
+	dname "RHYDON"
+	dname "KANGASKHAN"
+	dname "NIDORAN♂"
+	dname "CLEFAIRY"
+	dname "SPEAROW"
+	dname "VOLTORB"
+	dname "NIDOKING"
+	dname "SLOWBRO"
+	dname "IVYSAUR"
+	dname "EXEGGUTOR"
+	dname "LICKITUNG"
+	dname "EXEGGCUTE"
+	dname "GRIMER"
+	dname "GENGAR"
+	dname "NIDORAN♀"
+	dname "NIDOQUEEN"
+	dname "CUBONE"
+	dname "RHYHORN"
+	dname "LAPRAS"
+	dname "ARCANINE"
+	dname "MEW"
+	dname "GYARADOS"
+	dname "SHELLDER"
+	dname "TENTACOOL"
+	dname "GASTLY"
+	dname "SCYTHER"
+	dname "STARYU"
+	dname "BLASTOISE"
+	dname "PINSIR"
+	dname "TANGELA"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "GROWLITHE"
+	dname "ONIX"
+	dname "FEAROW"
+	dname "PIDGEY"
+	dname "SLOWPOKE"
+	dname "KADABRA"
+	dname "GRAVELER"
+	dname "CHANSEY"
+	dname "MACHOKE"
+	dname "MR.MIME"
+	dname "HITMONLEE"
+	dname "HITMONCHAN"
+	dname "ARBOK"
+	dname "PARASECT"
+	dname "PSYDUCK"
+	dname "DROWZEE"
+	dname "GOLEM"
+	dname "MISSINGNO."
+	dname "MAGMAR"
+	dname "MISSINGNO."
+	dname "ELECTABUZZ"
+	dname "MAGNETON"
+	dname "KOFFING"
+	dname "MISSINGNO."
+	dname "MANKEY"
+	dname "SEEL"
+	dname "DIGLETT"
+	dname "TAUROS"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "FARFETCH'D"
+	dname "VENONAT"
+	dname "DRAGONITE"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "DODUO"
+	dname "POLIWAG"
+	dname "JYNX"
+	dname "MOLTRES"
+	dname "ARTICUNO"
+	dname "ZAPDOS"
+	dname "DITTO"
+	dname "MEOWTH"
+	dname "KRABBY"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "VULPIX"
+	dname "NINETALES"
+	dname "PIKACHU"
+	dname "RAICHU"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "DRATINI"
+	dname "DRAGONAIR"
+	dname "KABUTO"
+	dname "KABUTOPS"
+	dname "HORSEA"
+	dname "SEADRA"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "SANDSHREW"
+	dname "SANDSLASH"
+	dname "OMANYTE"
+	dname "OMASTAR"
+	dname "JIGGLYPUFF"
+	dname "WIGGLYTUFF"
+	dname "EEVEE"
+	dname "FLAREON"
+	dname "JOLTEON"
+	dname "VAPOREON"
+	dname "MACHOP"
+	dname "ZUBAT"
+	dname "EKANS"
+	dname "PARAS"
+	dname "POLIWHIRL"
+	dname "POLIWRATH"
+	dname "WEEDLE"
+	dname "KAKUNA"
+	dname "BEEDRILL"
+	dname "MISSINGNO."
+	dname "DODRIO"
+	dname "PRIMEAPE"
+	dname "DUGTRIO"
+	dname "VENOMOTH"
+	dname "DEWGONG"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "CATERPIE"
+	dname "METAPOD"
+	dname "BUTTERFREE"
+	dname "MACHAMP"
+	dname "MISSINGNO."
+	dname "GOLDUCK"
+	dname "HYPNO"
+	dname "GOLBAT"
+	dname "MEWTWO"
+	dname "SNORLAX"
+	dname "MAGIKARP"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MUK"
+	dname "MISSINGNO."
+	dname "KINGLER"
+	dname "CLOYSTER"
+	dname "MISSINGNO."
+	dname "ELECTRODE"
+	dname "CLEFABLE"
+	dname "WEEZING"
+	dname "PERSIAN"
+	dname "MAROWAK"
+	dname "MISSINGNO."
+	dname "HAUNTER"
+	dname "ABRA"
+	dname "ALAKAZAM"
+	dname "PIDGEOTTO"
+	dname "PIDGEOT"
+	dname "STARMIE"
+	dname "BULBASAUR"
+	dname "VENUSAUR"
+	dname "TENTACRUEL"
+	dname "MISSINGNO."
+	dname "GOLDEEN"
+	dname "SEAKING"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "PONYTA"
+	dname "RAPIDASH"
+	dname "RATTATA"
+	dname "RATICATE"
+	dname "NIDORINO"
+	dname "NIDORINA"
+	dname "GEODUDE"
+	dname "PORYGON"
+	dname "AERODACTYL"
+	dname "MISSINGNO."
+	dname "MAGNEMITE"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "CHARMANDER"
+	dname "SQUIRTLE"
+	dname "CHARMELEON"
+	dname "WARTORTLE"
+	dname "CHARIZARD"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "ODDISH"
+	dname "GLOOM"
+	dname "VILEPLUME"
+	dname "BELLSPROUT"
+	dname "WEEPINBELL"
+	dname "VICTREEBEL"
 	assert_table_length NUM_POKEMON_INDEXES

Engezerstorung/pokered-gbc-plus-fr

data/events/trades.asm
@@ -1,7 +1,12 @@
+MACRO npctrade
+; give mon, get mon, dialog id, nickname
+	db \1, \2, \3
+	dname \4, NAME_LENGTH
+ENDM
+
 TradeMons:
 ; entries correspond to TRADE_FOR_* constants
 	table_width 3 + NAME_LENGTH
-	; give mon, get mon, dialog id, nickname
 	; The two instances of TRADE_DIALOGSET_EVOLUTION are a leftover
 	; from the Japanese Blue trades, which used species that evolve.
 	; TRADE_DIALOGSET_EVOLUTION did not refer to evolution in Japanese
@@ -9,14 +14,14 @@ TradeMons:
 	; "went and evolved" and also changed the trades to match. English
 	; Red/Blue uses the original JP Red/Green trades but with the JP Blue
 	; post-trade text.
-	db NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "BIBICHE@@@@"
-	db ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL@@@@@"
-	db BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU@" ; unused
-	db PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "BIBI@@@@@@@"
-	db SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "JULIO@@@@@@"
-	db SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "GLAVIOTEUR@"
-	db POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "NINI@@@@@@@"
-	db RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "KOURJUS@@@@"
-	db VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "BIGOUDI@@@@"
-	db NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "FABI@@@@@@@"
+	npctrade NIDORINO,   NIDORINA,  TRADE_DIALOGSET_CASUAL,    "BIBICHE"
+	npctrade ABRA,       MR_MIME,   TRADE_DIALOGSET_CASUAL,    "MARCEL"
+	npctrade BUTTERFREE, BEEDRILL,  TRADE_DIALOGSET_HAPPY,     "CHIKUCHIKU" ; unused
+	npctrade PONYTA,     SEEL,      TRADE_DIALOGSET_CASUAL,    "BIBI"
+	npctrade SPEAROW,    FARFETCHD, TRADE_DIALOGSET_HAPPY,     "JULIO"
+	npctrade SLOWBRO,    LICKITUNG, TRADE_DIALOGSET_CASUAL,    "GLAVIOTEUR"
+	npctrade POLIWHIRL,  JYNX,      TRADE_DIALOGSET_EVOLUTION, "NINI"
+	npctrade RAICHU,     ELECTRODE, TRADE_DIALOGSET_EVOLUTION, "KOURJUS"
+	npctrade VENONAT,    TANGELA,   TRADE_DIALOGSET_HAPPY,     "BIGOUDI"
+	npctrade NIDORAN_M,  NIDORAN_F, TRADE_DIALOGSET_HAPPY,     "FABI"
 	assert_table_length NUM_NPC_TRADES

data/pokemon/names.asm
@@ -1,193 +1,193 @@
 MonsterNames::
 	table_width NAME_LENGTH - 1
-	db "RHINOFEROS"
-	db "KANGOUREX@"
-	db "NIDORAN♂@@"
-	db "MELOFEE@@@"
-	db "PIAFABEC@@"
-	db "VOLTORBE@@"
-	db "NIDOKING@@"
-	db "FLAGADOSS@"
-	db "HERBIZARRE"
-	db "NOADKOKO@@"
-	db "EXCELANGUE"
-	db "NOEUNOEUF@"
-	db "TADMORV@@@"
-	db "ECTOPLASMA"
-	db "NIDORAN♀@@"
-	db "NIDOQUEEN@"
-	db "OSSELAIT@@"
-	db "RHINOCORNE"
-	db "LOKHLASS@@"
-	db "ARCANIN@@@"
-	db "MEW@@@@@@@"
-	db "LEVIATOR@@"
-	db "KOKIYAS@@@"
-	db "TENTACOOL@"
-	db "FANTOMINUS"
-	db "INSECATEUR"
-	db "STARI@@@@@"
-	db "TORTANK@@@"
-	db "SCARABRUTE"
-	db "SAQUEDENEU"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "CANINOS@@@"
-	db "ONIX@@@@@@"
-	db "RAPASDEPIC"
-	db "ROUCOOL@@@"
-	db "RAMOLOSS@@"
-	db "KADABRA@@@"
-	db "GRAVALANCH"
-	db "LEVEINARD@"
-	db "MACHOPEUR@"
-	db "M.MIME@@@@"
-	db "KICKLEE@@@"
-	db "TYGNON@@@@"
-	db "ARBOK@@@@@"
-	db "PARASECT@@"
-	db "PSYKOKWAK@"
-	db "SOPORIFIK@"
-	db "GROLEM@@@@"
-	db "MISSINGNO."
-	db "MAGMAR@@@@"
-	db "MISSINGNO."
-	db "ELEKTEK@@@"
-	db "MAGNETON@@"
-	db "SMOGO@@@@@"
-	db "MISSINGNO."
-	db "FEROSINGE@"
-	db "OTARIA@@@@"
-	db "TAUPIQUEUR"
-	db "TAUROS@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "CANARTICHO"
-	db "MIMITOSS@@"
-	db "DRACOLOSSE"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "DODUO@@@@@"
-	db "PTITARD@@@"
-	db "LIPPOUTOU@"
-	db "SULFURA@@@"
-	db "ARTIKODIN@"
-	db "ELECTHOR@@"
-	db "METAMORPH@"
-	db "MIAOUSS@@@"
-	db "KRABBY@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "GOUPIX@@@@"
-	db "FEUNARD@@@"
-	db "PIKACHU@@@"
-	db "RAICHU@@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MINIDRACO@"
-	db "DRACO@@@@@"
-	db "KABUTO@@@@"
-	db "KABUTOPS@@"
-	db "HYPOTREMPE"
-	db "HYPOCEAN@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "SABELETTE@"
-	db "SABLAIREAU"
-	db "AMONITA@@@"
-	db "AMONISTAR@"
-	db "RONDOUDOU@"
-	db "GRODOUDOU@"
-	db "EVOLI@@@@@"
-	db "PYROLI@@@@"
-	db "VOLTALI@@@"
-	db "AQUALI@@@@"
-	db "MACHOC@@@@"
-	db "NOSFERAPTI"
-	db "ABO@@@@@@@"
-	db "PARAS@@@@@"
-	db "TETARTE@@@"
-	db "TARTARD@@@"
-	db "ASPICOT@@@"
-	db "COCONFORT@"
-	db "DARDARGNAN"
-	db "MISSINGNO."
-	db "DODRIO@@@@"
-	db "COLOSSINGE"
-	db "TRIOPIKEUR"
-	db "AEROMITE@@"
-	db "LAMANTINE@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "CHENIPAN@@"
-	db "CHRYSACIER"
-	db "PAPILUSION"
-	db "MACKOGNEUR"
-	db "MISSINGNO."
-	db "AKWAKWAK@@"
-	db "HYPNOMADE@"
-	db "NOSFERALTO"
-	db "MEWTWO@@@@"
-	db "RONFLEX@@@"
-	db "MAGICARPE@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "GROTADMORV"
-	db "MISSINGNO."
-	db "KRABBOSS@@"
-	db "CRUSTABRI@"
-	db "MISSINGNO."
-	db "ELECTRODE@"
-	db "MELODELFE@"
-	db "SMOGOGO@@@"
-	db "PERSIAN@@@"
-	db "OSSATUEUR@"
-	db "MISSINGNO."
-	db "SPECTRUM@@"
-	db "ABRA@@@@@@"
-	db "ALAKAZAM@@"
-	db "ROUCOUPS@@"
-	db "ROUCARNAGE"
-	db "STAROSS@@@"
-	db "BULBIZARRE"
-	db "FLORIZARRE"
-	db "TENTACRUEL"
-	db "MISSINGNO."
-	db "POISSIRENE"
-	db "POISSOROY@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "PONYTA@@@@"
-	db "GALOPA@@@@"
-	db "RATTATA@@@"
-	db "RATTATAC@@"
-	db "NIDORINO@@"
-	db "NIDORINA@@"
-	db "RACAILLOU@"
-	db "PORYGON@@@"
-	db "PTERA@@@@@"
-	db "MISSINGNO."
-	db "MAGNETI@@@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "SALAMECHE@"
-	db "CARAPUCE@@"
-	db "REPTINCEL@"
-	db "CARABAFFE@"
-	db "DRACAUFEU@"
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MISSINGNO."
-	db "MYSTHERBE@"
-	db "ORTIDE@@@@"
-	db "RAFFLESIA@"
-	db "CHETIFLOR@"
-	db "BOUSTIFLOR"
-	db "EMPIFLOR@@"
+	dname "RHINOFEROS"
+	dname "KANGOUREX"
+	dname "NIDORAN♂"
+	dname "MELOFEE"
+	dname "PIAFABEC"
+	dname "VOLTORBE"
+	dname "NIDOKING"
+	dname "FLAGADOSS"
+	dname "HERBIZARRE"
+	dname "NOADKOKO"
+	dname "EXCELANGUE"
+	dname "NOEUNOEUF"
+	dname "TADMORV"
+	dname "ECTOPLASMA"
+	dname "NIDORAN♀"
+	dname "NIDOQUEEN"
+	dname "OSSELAIT"
+	dname "RHINOCORNE"
+	dname "LOKHLASS"
+	dname "ARCANIN"
+	dname "MEW"
+	dname "LEVIATOR"
+	dname "KOKIYAS"
+	dname "TENTACOOL"
+	dname "FANTOMINUS"
+	dname "INSECATEUR"
+	dname "STARI"
+	dname "TORTANK"
+	dname "SCARABRUTE"
+	dname "SAQUEDENEU"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "CANINOS"
+	dname "ONIX"
+	dname "RAPASDEPIC"
+	dname "ROUCOOL"
+	dname "RAMOLOSS"
+	dname "KADABRA"
+	dname "GRAVALANCH"
+	dname "LEVEINARD"
+	dname "MACHOPEUR"
+	dname "M.MIME"
+	dname "KICKLEE"
+	dname "TYGNON"
+	dname "ARBOK"
+	dname "PARASECT"
+	dname "PSYKOKWAK"
+	dname "SOPORIFIK"
+	dname "GROLEM"
+	dname "MISSINGNO."
+	dname "MAGMAR"
+	dname "MISSINGNO."
+	dname "ELEKTEK"
+	dname "MAGNETON"
+	dname "SMOGO"
+	dname "MISSINGNO."
+	dname "FEROSINGE"
+	dname "OTARIA"
+	dname "TAUPIQUEUR"
+	dname "TAUROS"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "CANARTICHO"
+	dname "MIMITOSS"
+	dname "DRACOLOSSE"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "DODUO"
+	dname "PTITARD"
+	dname "LIPPOUTOU"
+	dname "SULFURA"
+	dname "ARTIKODIN"
+	dname "ELECTHOR"
+	dname "METAMORPH"
+	dname "MIAOUSS"
+	dname "KRABBY"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "GOUPIX"
+	dname "FEUNARD"
+	dname "PIKACHU"
+	dname "RAICHU"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MINIDRACO"
+	dname "DRACO"
+	dname "KABUTO"
+	dname "KABUTOPS"
+	dname "HYPOTREMPE"
+	dname "HYPOCEAN"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "SABELETTE"
+	dname "SABLAIREAU"
+	dname "AMONITA"
+	dname "AMONISTAR"
+	dname "RONDOUDOU"
+	dname "GRODOUDOU"
+	dname "EVOLI"
+	dname "PYROLI"
+	dname "VOLTALI"
+	dname "AQUALI"
+	dname "MACHOC"
+	dname "NOSFERAPTI"
+	dname "ABO"
+	dname "PARAS"
+	dname "TETARTE"
+	dname "TARTARD"
+	dname "ASPICOT"
+	dname "COCONFORT"
+	dname "DARDARGNAN"
+	dname "MISSINGNO."
+	dname "DODRIO"
+	dname "COLOSSINGE"
+	dname "TRIOPIKEUR"
+	dname "AEROMITE"
+	dname "LAMANTINE"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "CHENIPAN"
+	dname "CHRYSACIER"
+	dname "PAPILUSION"
+	dname "MACKOGNEUR"
+	dname "MISSINGNO."
+	dname "AKWAKWAK"
+	dname "HYPNOMADE"
+	dname "NOSFERALTO"
+	dname "MEWTWO"
+	dname "RONFLEX"
+	dname "MAGICARPE"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "GROTADMORV"
+	dname "MISSINGNO."
+	dname "KRABBOSS"
+	dname "CRUSTABRI"
+	dname "MISSINGNO."
+	dname "ELECTRODE"
+	dname "MELODELFE"
+	dname "SMOGOGO"
+	dname "PERSIAN"
+	dname "OSSATUEUR"
+	dname "MISSINGNO."
+	dname "SPECTRUM"
+	dname "ABRA"
+	dname "ALAKAZAM"
+	dname "ROUCOUPS"
+	dname "ROUCARNAGE"
+	dname "STAROSS"
+	dname "BULBIZARRE"
+	dname "FLORIZARRE"
+	dname "TENTACRUEL"
+	dname "MISSINGNO."
+	dname "POISSIRENE"
+	dname "POISSOROY"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "PONYTA"
+	dname "GALOPA"
+	dname "RATTATA"
+	dname "RATTATAC"
+	dname "NIDORINO"
+	dname "NIDORINA"
+	dname "RACAILLOU"
+	dname "PORYGON"
+	dname "PTERA"
+	dname "MISSINGNO."
+	dname "MAGNETI"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "SALAMECHE"
+	dname "CARAPUCE"
+	dname "REPTINCEL"
+	dname "CARABAFFE"
+	dname "DRACAUFEU"
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MISSINGNO."
+	dname "MYSTHERBE"
+	dname "ORTIDE"
+	dname "RAFFLESIA"
+	dname "CHETIFLOR"
+	dname "BOUSTIFLOR"
+	dname "EMPIFLOR"
 	assert_table_length NUM_POKEMON_INDEXES