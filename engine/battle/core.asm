pret/pokered

Use `ld_hli_a_string` macro to make byte-by-byte strings more obvious (#540)

engine/battle/core.asm
@@ -6841,17 +6841,7 @@ InitWildBattle:
 	ld [hli], a   ; write front sprite pointer
 	ld [hl], b
 	ld hl, wEnemyMonNick  ; set name to "GHOST"
-	ld a, 'G'
-	ld [hli], a
-	ld a, 'H'
-	ld [hli], a
-	ld a, 'O'
-	ld [hli], a
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'T'
-	ld [hli], a
-	ld [hl], '@'
+	ld_hli_a_string "GHOST@"
 	ld a, [wCurPartySpecies]
 	push af
 	ld a, MON_GHOST

Engezerstorung/pokered-gbc-plus-fr

engine/battle/core.asm
@@ -6884,21 +6884,7 @@ InitWildBattle:
 	ld [hli], a   ; write front sprite pointer
 	ld [hl], b
 	ld hl, wEnemyMonNick  ; set name to "GHOST"
-	ld a, 'S'
-	ld [hli], a
-	ld a, 'P'
-	ld [hli], a
-	ld a, 'E'
-	ld [hli], a
-	ld a, 'C'
-	ld [hli], a
-	ld a, 'T'
-	ld [hli], a
-	ld a, 'R'
-	ld [hli], a
-	ld a, 'E'
-	ld [hli], a
-	ld [hl], '@'
+	ld_hli_a_string "SPECTRE@"
 	ld a, [wCurPartySpecies]
 	push af
 	ld a, MON_GHOST